793f0c3a3c55c545e2afe61e1434ef16e83b0e85
moytri <moytritopa@gmail.com>
2018-11-26 19:31:08 -0800

    add AuthBean for login and logout

---
+++ jsfCrud/src/main/java/ca/bcit/com4613/bean/AuthBean.java
@@ -0,0 +1,34 @@
+package ca.bcit.com4613.bean;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class AuthBean implements Serializable{	
+
+	private static final long serialVersionUID = 1L;
+
+	public void redirect() throws IOException{
+		
+	    ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+
+	    if (context.isUserInRole("admin")){
+	        context.redirect("admin/index.xhtml");
+	    }
+	    else {
+	        context.redirect("user/index.xhtml");
+	    }
+	}
+	
+	public void logout() throws IOException {
+	    ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+	    ec.invalidateSession();
+	    ec.redirect(ec.getRequestContextPath() + "/faces/index.xhtml");
+	}	
+}
